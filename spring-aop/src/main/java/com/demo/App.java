79111661c97ebc2bf90cb6fe074794e899184512
Soonki Ji <soonki.ji@gmail.com>
2017-03-30 19:34:26 -0400

    sprint aop demo

---
+++ spring-aop/src/main/java/com/demo/App.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import com.demo.service.EchoService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        EchoService echoService = (EchoService) ac.getBean("echoServiceProxy");
+        String result = echoService.echo("One");
+        System.out.println(result);
+    }
+}
